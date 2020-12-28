efe1c3793a04b9317e819e4353ed827d06e76f6f
Ben Liu <myme5261314@gmail.com>
2020-12-28 19:42:03 +0800

    Replace all the TABs in the python file into 4 spaces. Remove a syntax error.

--- example.py
+++ example.py
@@ -5,23 +5,23 @@ from appstoreconnect import Api
 
 
 if __name__ == "__main__":
-	key_id = sys.argv[1]
-	key_file = sys.argv[2]
-	issuer_id = sys.argv[3]
-	api = Api(key_id, key_file, issuer_id)
+    key_id = sys.argv[1]
+    key_file = sys.argv[2]
+    issuer_id = sys.argv[3]
+    api = Api(key_id, key_file, issuer_id)
 
-	# list all apps
-	apps = api.list_apps()
-	for app in apps:
-		print(app.name, app.sku)
+    # list all apps
+    apps = api.list_apps()
+    for app in apps:
+        print(app.name, app.sku)
 
-	# filter apps
-	apps = api.list_apps(filters={'sku': 'DINORUSH', 'name': 'Dino Rush'})
-	print("%d apps found" % len(apps))
+    # filter apps
+    apps = api.list_apps(filters={'sku': 'DINORUSH', 'name': 'Dino Rush'})
+    print("%d apps found" % len(apps))
 
-	# download sales report
-	api.download_sales_and_trends_reports(
-		filters={'vendorNumber': '123456789', 'frequency': 'WEEKLY', 'reportDate': '2019-06-09'}, save_to='report.csv')
+    # download sales report
+    api.download_sales_and_trends_reports(
+        filters={'vendorNumber': '123456789', 'frequency': 'WEEKLY', 'reportDate': '2019-06-09'}, save_to='report.csv')
 
-	# download finance report
-	api.download_finance_reports(filters={'vendorNumber': '123456789', 'reportDate': '2019-06'}, save_to='finance.csv')
+    # download finance report
+    api.download_finance_reports(filters={'vendorNumber': '123456789', 'reportDate': '2019-06'}, save_to='finance.csv')
